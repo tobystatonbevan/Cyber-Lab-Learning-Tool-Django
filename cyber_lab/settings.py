26c2fb7a6bebe7ba798008d0668537df5b0e1145
tobystatonbevan <154239390+tobystatonbevan@users.noreply.github.com>
2024-05-27 15:40:05 +0100

    admin portal fix

--- cyber_lab/settings.py
+++ cyber_lab/settings.py
@@ -35,13 +35,13 @@ INSTALLED_APPS = [
     'tasks.apps.TasksConfig',
     'assignments.apps.AssignmentsConfig',
     'lessons.apps.LessonsConfig',
-    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
