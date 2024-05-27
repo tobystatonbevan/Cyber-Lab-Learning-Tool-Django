cbba168766a2cc9baa7658286df312c64c21807a
tobystatonbevan <154239390+tobystatonbevan@users.noreply.github.com>
2024-05-27 14:15:31 +0100

    started log in implementation

--- cyber_lab/urls.py
+++ cyber_lab/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.contrib import admin,auth
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
@@ -25,4 +25,5 @@ urlpatterns = [
     path("tasks/",include("tasks.urls")),
     path("lessons/",include("lessons.urls")),
     path("assignments/",include("assignments.urls")),
+    path("accounts/", auth.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
